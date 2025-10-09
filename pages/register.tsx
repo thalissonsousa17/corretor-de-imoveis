6b1293e8affc00433137195fad29660cdb71575f
Automatech <thalissonsousa17@gmail.com>
2025-10-09 19:14:58 -0300

    feat(frontend): implementa as paginas de login e registro, integrando com as APIs de autenticacao

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,32 +1,42 @@
-import { useAuth } from "@/lib/AuthContext";
 import axios from "axios";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
-const LoginPage = () => {
+const RegisterPage = () => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
+  const [message, setMessage] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const { login } = useAuth();
   const router = useRouter();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setIsLoading(true);
     setError(null);
+    setMessage(null);
 
     try {
-      const response = await axios.post("/api/login", { email, password });
+      const response = await fetch("/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password }),
+      });
 
-      if (response.status === 200) {
-        login(response.data.user);
-        router.push("/corretor/dashboard");
+      if (response.status === 201) {
+        setMessage("Registro bem-sucedido! Redirecionando para login...");
+        setTimeout(() => {
+          router.push("/login");
+        }, 2000);
       }
     } catch (err) {
       if (axios.isAxiosError(err) && err.response) {
-        setError(err.response.data.message || "Ocorreu um erro ao fazer no login.");
+        setError(err.response.data.message || "Ocorreu um erro ao registrar.");
       } else {
         setError("Ocorreu um erro desconhecido. Tente novamente.");
       }
@@ -34,15 +44,31 @@ const LoginPage = () => {
       setIsLoading(false);
     }
   };
-
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-gray-800">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded shadow">
-        <h1 className="text-2xl font-bold text-center text-gray-700">Acessar sua Conta</h1>
-        <form onSubmit={handleSubmit} className="space-y-4">
+        <h1 className="text-2xl font-bold text-center text-gray-700">Criar Conta</h1>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          {/* nome */}
           <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-              Email
+            <label htmlFor="name" className="text-sm font-medium text-gray-700">
+              Nome Completo
+            </label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Digite seu nome completo"
+              className="text-gray-500 mt-1 block w-full px-3 py-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+          {/* e-mail */}
+          <div>
+            <label htmlFor="email" className="text-sm font-medium text-gray-700">
+              E-mail
             </label>
             <input
               id="email"
@@ -51,12 +77,14 @@ const LoginPage = () => {
               required
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              placeholder="Digite seu email"
+              placeholder="Digite seu e-mail"
               className="text-gray-500 mt-1 block w-full px-3 py-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             />
           </div>
+
+          {/* senha */}
           <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+            <label htmlFor="password" className="text-sm font-medium text-gray-700">
               Senha
             </label>
             <input
@@ -70,20 +98,29 @@ const LoginPage = () => {
               className="text-gray-500 mt-1 block w-full px-3 py-2 border border-gray-300 rounded shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             />
           </div>
+          {/* exibir mensagem */}
+
           {error && <p className="text-red-600 text-sm mb-4">{error}</p>}
+          {message && <p className="text-green-600 text-sm mb-4">{message}</p>}
+
           <div>
             <button
               type="submit"
               disabled={isLoading}
               className="w-full  flex justify-center py-2 px-4 border border-transparent rounded shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50"
             >
-              {isLoading ? "Entrando..." : "Entrar"}
+              {isLoading ? "Registrando..." : "Registrar"}
             </button>
           </div>
         </form>
+        <p className=" flex justify-center items-center">
+          Já tem conta ?
+          <Link href="/login" className="font-medium text-indigo-600 hover:text-indigo-500 ">
+            Fazer Login
+          </Link>
+        </p>
       </div>
     </div>
   );
 };
-
-export default LoginPage;
+export default RegisterPage;
