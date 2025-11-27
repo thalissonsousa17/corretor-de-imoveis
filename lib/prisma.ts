56ed1f60f66d88d136bc733c7ce1b2554f29cb61
Automatech <thalissonsousa17@gmail.com>
2025-11-27 15:38:37 -0300

    altera lib/prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
