cf2a29ccaeef21447577759e8902a3666e812bd8
Automatech <thalissonsousa17@gmail.com>
2025-12-29 17:43:16 -0300

    WIP

--- lib/cookies.ts
+++ lib/cookies.ts
@@ -3,6 +3,6 @@ import type { SerializeOptions } from "cookie";
 export const cookieOptions: SerializeOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  sameSite: "lax",
   path: "/",
 };
