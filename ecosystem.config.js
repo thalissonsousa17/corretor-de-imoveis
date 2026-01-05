d2f36cdc650d7438e1732463ed9df280846a88ce
Automatech <thalissonsousa17@gmail.com>
2026-01-05 15:36:53 -0300

    feat: port:400

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "next-app",
       script: "npm",
-      args: "run start",
+      args: "run start -- -p 4000",
       cwd: "/projects/corretor-de-imoveis",
       env_file: ".env",
     },
