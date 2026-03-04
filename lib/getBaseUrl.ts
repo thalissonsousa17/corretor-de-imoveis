11d17ed1ae1232c26f058f2362b1f1403f29bb78
Automatech <thalissonsousa17@gmail.com>
2026-03-03 22:24:24 -0300

    WIP

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -2,5 +2,11 @@ export function GetBaseUrl() {
   if (typeof window !== "undefined") {
     return window.location.origin;
   }
+
+  // Vercel injeta automaticamente VERCEL_URL (sem https://)
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
   return process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 }
